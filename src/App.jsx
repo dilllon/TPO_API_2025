dfa410d12b3765add4f5fa805de3ae75f695714e
Naka <tomas.nakasone@arreglatechcompany.com>
2025-08-08 21:44:52 -0300

    feat: Added mock register view, react router and redux for states

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/views/Home/Home';
+import Register from './components/views/Register/Register';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
