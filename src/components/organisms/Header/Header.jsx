aff502c0a7f234a862e516a8d2a92687166b39fd
Axel Higa <axeltomashiga@gmail.com>
2025-08-11 09:05:54 -0300

    change header, module Navbar and responsive

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,30 +1,12 @@
 import './Header.css';
+import '../../molecules/NavBar/NavBar'
+import NavBar from '../../molecules/NavBar/NavBar';
 
 function Header() {
   return (
-    <header className="header">
-      <h1 className="titulo">Tienda Online</h1>
-
-      <div className="buscador">
-        <input
-          type="text"
-          placeholder="Buscar productos..."
-          className="input-busqueda"
-        />
-        <button className="btn-buscar">Buscar</button>
-      </div>
-      <nav className="nav">
-        <a href="#" className="nav-link">
-          Inicio
-        </a>
-        <a href="#" className="nav-link">
-          Catálogo
-        </a>
-        <a href="#" className="nav-link">
-          Login
-        </a>
-      </nav>
-    </header>
+  <header className="header">
+    <NavBar/>
+  </header>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
