2c395c57098e6690ecf3cd95996db84d135a810b
pensadoivan <ivanpensado7@gmail.com>
2025-08-13 13:07:23 -0300

    product grid con funcion map

--- src/components/organisms/Grid/Productos.jsx
+++ src/components/organisms/Grid/Productos.jsx
@@ -1,38 +1,98 @@
-import { useState } from 'react';
-import ProductoCard from '@/components/molecules/ProductCard/ProductCard';
-import './Productos.css';
+import React from "react";
+import ProductoCard from "@/components/molecules/ProductCard/ProductCard";
+import "./Productos.css";
 
 function ProductosGrid() {
-  return (
-    <div className="productos-grid container-fluid">
-        <div className="row align-content-center">
-            <div className="col-12 col-md-6 col-xl-4 d-flex justify-content-center">
-                <ProductoCard
-                    titulo="Mouse inalámbrico"
-                    precio={2999}
-                    imagen="https://dummyimage.com/200x200/cccccc/000000.png&text=Producto"
-                    stock={0}
-                />
-            </div>
-            <div className="col-12 col-md-6 col-xl-4 d-flex justify-content-center">
-                <ProductoCard
-                    titulo="Mouse inalámbrico"
-                    precio={2999}
-                    imagen="https://dummyimage.com/200x200/cccccc/000000.png&text=Producto"
-                    stock={0}
-                />
-            </div>
-            <div className="col-12 col-md-6 col-xl-4 d-flex justify-content-center">
-                <ProductoCard
-                    titulo="Mouse inalámbrico"
-                    precio={2999}
-                    imagen="https://dummyimage.com/200x200/cccccc/000000.png&text=Producto"
-                    stock={0}
-                />
-            </div>
-        </div>
+  const productoscat1 = [
+    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    },
+    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    },
+    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    },    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    },    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    }
+  ];
+
+
+
+
+  const productoscat2 = [
+    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    },
+    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    },
+    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    },    {
+      titulo: "Mouse inalámbrico",
+      precio: 2999,
+      imagen: "https://dummyimage.com/200x200/cccccc/000000.png&text=Producto",
+      stock: 0
+    }
+  ];
+
+ return (
+  <>
+    {/* Primer bloque */}
+    <h2>Categoria 1</h2>
+    <div className="productos-grid">
+      {productoscat1.map((p, i) => (
+        <ProductoCard
+          key={`cat1-${i}`}
+          titulo={p.titulo}
+          precio={p.precio}
+          imagen={p.imagen}
+          stock={p.stock}
+        />
+      ))}
     </div>
-  );
+    <h2>Categoria 2</h2>
+    {/* Segundo bloque */}
+    <div className="productos-grid">
+      {productoscat2.map((p, i) => (
+        <ProductoCard
+          key={`cat2-${i}`}
+          titulo={p.titulo}
+          precio={p.precio}
+          imagen={p.imagen}
+          stock={p.stock}
+        />
+      ))}
+    </div>
+  </>
+);
 }
 
 export default ProductosGrid;
