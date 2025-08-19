3c85e3330e955fff9e0ff7de0c65baca1dee5c72
Axel Higa <axeltomashiga@gmail.com>
2025-08-19 16:31:07 -0300

    add form responsive

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -12,7 +12,7 @@ function Logo() {
         <path d="M7 4h-2l-3 9v2a1 1 0 001 1h1a4 4 0 108 0h1a1 1 0 001-1v-2l-3-9h-2l1 3h-4l1-3zm-1 14a2 2 0 110-4 2 2 0 010 4zm10-2a2 2 0 11-4 0 2 2 0 014 0z"/>
       </svg>
       <span className="logo-text">
-        Tienda<span className="logo-highlight">Online</span>
+        Ama<span className="logo-highlight">Zone</span>
       </span>
     </a>
   );
