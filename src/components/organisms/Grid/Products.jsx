f58669d7de6ffaeddbebd29bc09ab27c0ba504f0
pensadoivan <ivanpensado7@gmail.com>
2025-08-20 16:04:54 -0300

    Carousel basico en productos

--- src/components/organisms/Grid/Products.jsx
+++ src/components/organisms/Grid/Products.jsx
@@ -2,6 +2,7 @@ import ProductCard from "@/components/molecules/ProductCard/ProductCard";
 import { useMemo } from "react";
 import { getProductsGroupedByCategory } from "../../../constants/products";
 import "./Products.css";
+import CarouselProducts from "@/components/molecules/CarouselProducts/CarouselProducts";
 
 // Ordena por título (ES-AR), devolviendo una COPIA
 function orderProducts(products) {
@@ -14,34 +15,36 @@ function orderProducts(products) {
 }
 
 function ProductsGrid({ onClick }) {
-  // Separa por categoria y luego Ordena los productos de cada categoría
-  const ordered = useMemo(
-    () => {
-      const categories = getProductsGroupedByCategory();
-      return categories.map((cat) => ({
-        ...cat,
-        products: orderProducts(cat.products),
-      }));
-    },
-    []
-  );
+  // Separa por categoría y ordena los productos de cada categoría
+  const ordered = useMemo(() => {
+    const categories = getProductsGroupedByCategory();
+    return categories.map((cat) => ({
+      ...cat,
+      products: orderProducts(cat.products),
+    }));
+  }, []);
 
   return (
     <>
       {ordered.map(({ categoryName, products }) => (
-      
-        <section key={categoryName} id={`category-${categoryName.toLowerCase()}`} className="products-section">
+        <section
+          key={categoryName}
+          id={`category-${categoryName.toLowerCase()}`}
+          className="products-section"
+        >
           <h2>{categoryName}</h2>
-          <div className="products-grid">
-            {products.map((p) => (
+
+          <CarouselProducts
+            products={products}
+            renderCard={(p) => (
               <ProductCard
                 key={p.id}
                 product={p}
-                variant={categoryName}          // por si querés estilos/behaviour por categoría
-                onClick={() => onClick(p)}      // agrega al carrito
+                variant={categoryName}         // por si querés estilos por categoría
+                onClick={() => onClick?.(p)}   // agrega al carrito si se pasó prop
               />
-            ))}
-          </div>
+            )}
+          />
         </section>
       ))}
     </>
