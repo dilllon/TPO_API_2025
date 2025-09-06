690d8e2479de212a239358358beb322064e8ebf0
dilllon <luciodillon2001@gmail.com>
2025-09-06 19:01:00 -0300

    chua redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
-import { store } from './store/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Provider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 );
