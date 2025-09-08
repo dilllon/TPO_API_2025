06f32f695a4e5a75b7629a0450ee1ed43fa5e4bb
Naka <tomas.nakasone@arreglatechcompany.com>
2025-09-08 18:04:18 -0300

    AddFavs: added fav btn, global context and populated fav dropdown

--- src/components/molecules/ProductCard/ProductCard.jsx
+++ src/components/molecules/ProductCard/ProductCard.jsx
@@ -1,13 +1,17 @@
-import { FaEdit } from 'react-icons/fa';
+import { FaEdit, FaHeart } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useProducts } from '../../../context/ProductContext';
 import { useUser } from '../../../context/UserContext';
+import { useFavorites } from '../../../hooks/useFavorite';
 import './ProductCard.css';
 
-function ProductCard({ product, onClick, variant = "default" }) {
+function ProductCard({ product, onClick, variant = 'default' }) {
   const { calculateDiscountedPrice, hasDiscount, canEdit } = useProducts();
   const navigate = useNavigate();
   const { userData, isAuthenticated } = useUser();
+  const { addToFavorites, favorites } = useFavorites();
+
+  const isFavorite = favorites.some((fav) => fav.id === product.id);
 
   const handleProductClick = () => {
     navigate(`/products/${product.id}`);
@@ -15,7 +19,7 @@ function ProductCard({ product, onClick, variant = "default" }) {
 
   const onAddToCart = (e) => {
     e.stopPropagation(); // Evita que se active la navegación
-    
+
     if (onClick) {
       onClick(product);
     }
@@ -24,7 +28,12 @@ function ProductCard({ product, onClick, variant = "default" }) {
   const handleEdit = (e) => {
     e.stopPropagation();
     navigate(`/products/${product.id}/edit`);
-  }
+  };
+
+  const handleAddToFavorites = (e) => {
+    e.stopPropagation();
+    addToFavorites(product);
+  };
 
   const productHasDiscount = hasDiscount(product);
   const discountedPrice = calculateDiscountedPrice(product);
@@ -32,18 +41,22 @@ function ProductCard({ product, onClick, variant = "default" }) {
   return (
     <div className={`product-card ${variant}`}>
       {productHasDiscount && (
-        <div className="discount-badge">
-          -{product.discount}%
-        </div>
+        <div className="discount-badge">-{product.discount}%</div>
       )}
       <div className="product-clickable-area" onClick={handleProductClick}>
         <h3 className="product-title">{product.title}</h3>
-        <img src={product.image} alt={product.title} className="product-image" />
+        <img
+          src={product.image}
+          alt={product.title}
+          className="product-image"
+        />
         <div className="price-container">
           {productHasDiscount ? (
             <>
               <p className="product-price original-price">${product.price}</p>
-              <p className="product-price discounted-price">${discountedPrice}</p>
+              <p className="product-price discounted-price">
+                ${discountedPrice}
+              </p>
             </>
           ) : (
             <p className="product-price">${product.price}</p>
@@ -51,12 +64,26 @@ function ProductCard({ product, onClick, variant = "default" }) {
         </div>
         <p className="product-stock">
           Stock:{' '}
-          {product.stock > 0 ? product.stock : <span className="out-of-stock">Sin stock</span>}
+          {product.stock > 0 ? (
+            product.stock
+          ) : (
+            <span className="out-of-stock">Sin stock</span>
+          )}
         </p>
       </div>
-      <div className='product-btns'>
+      <div className="product-btns">
         <button onClick={onAddToCart}>Agregar al carrito</button>
-        {isAuthenticated && canEdit(product.id, userData.id) && <button onClick={handleEdit} className='edit-button'><FaEdit /></button>}
+        <button
+          onClick={handleAddToFavorites}
+          className={`favorite-button ${isFavorite ? 'favorited' : ''}`}
+        >
+          <FaHeart />
+        </button>
+        {isAuthenticated && canEdit(product.id, userData.id) && (
+          <button onClick={handleEdit} className="edit-button">
+            <FaEdit />
+          </button>
+        )}
       </div>
     </div>
   );
