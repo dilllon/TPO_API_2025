1de4743dc98ce22b6bc47adab188092c74ce3f25
pensadoivan <ivanpensado7@gmail.com>
2025-10-20 14:50:45 -0300

    ajuste enlace

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -69,8 +69,14 @@ export function UserProvider({ children }) {
   const login = async (username=null, email=null, password) => {
     try {
       setIsLoading(true);
-      const response = username ? await fetch(`http://localhost:8080/user?username=${username}&password=${password}`) : await fetch(`http://localhost:8080/user?email=${email}&password=${password}`);
-
+      const response = await fetch('http://localhost:8080/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+});
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
